257ec66b8f5184d177212246ec7e50a9726fe606
SongOTS <jonafriansyah3@gmail.com>
2024-10-29 23:17:10 +0800

    changes

--- Dashboard/dashboard.py
+++ Dashboard/dashboard.py
@@ -66,13 +66,15 @@ with st.sidebar:
     st.image("https://github.com/siren-song-of-war/Submission/blob/main/c8da0175e9dba0109e4baaca8753b277.png?raw=true")
     
     # Mengambil start_date & end_date dari date_input
-    start_date, end_date = st.date_input(
-        label='Rentang Waktu',min_value=min_date,
-        max_value=max_date,
-        value=[min_date, max_date]
+    try:
+        start_date, end_date = st.date_input(
+            label='Rentang Waktu',min_value=min_date,
+            max_value=max_date,
+            value=[min_date, max_date]
     )
+    except ValueError:
 
-main_df = all_df[(all_df["order_approved_at"] >= str(start_date)) & 
+        main_df = all_df[(all_df["order_approved_at"] >= str(start_date)) & 
                 (all_df["order_approved_at"] <= str(end_date))]
 
 daily_orders_df = create_daily_orders_df(main_df)
